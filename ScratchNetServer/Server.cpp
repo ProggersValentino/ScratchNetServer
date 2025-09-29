f236559b1f1515707540836c00027671e665d632
optional <neonerbeni@gmail.com>
2025-09-30 07:47:51 +1000

    fdf

--- ScratchNetServer/Server.cpp
+++ ScratchNetServer/Server.cpp
@@ -1,9 +1,12 @@
 #include "Server.h"
 #include "Address.h"
 
+#pragma comment(lib, "zlib.lib")
+
 void Server::MainProcess()
 {
-    
+    playerAddress.reserve(maxPlayers); //assign amount of players
+    playerConnected.reserve(maxPlayers); //assign amount of players
 
     listeningSocket = *RetrieveSocket(); //create new socket
     listeningSocket.OpenSock(30000, true);
@@ -36,4 +39,63 @@ void Server::MainProcess()
             }
         }
     }
+}
+
+int Server::FindPlayer(Address player)
+{
+    for (int i = 0; i < maxPlayers; i++)
+    {
+        if (playerConnected[i] && playerAddress[i] == player) 
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int Server::FindFreeClientIndex() const
+{
+    for (int i = 0; i < maxPlayers; i++)
+    {
+        if (!playerConnected[i]) //if there is a free spot the playerconnected array will have a spot that is set to false
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool Server::IsClientConnected(int clientIndex)
+{
+    return playerConnected[clientIndex];
+}
+
+const Address& Server::GetClientAddress(int clientIndex)
+{
+    return playerAddress[clientIndex];
+}
+
+bool Server::TryToAddPlayer(Address potentialPlayer)
+{
+    int playerLoco = FindPlayer(potentialPlayer);
+
+    if (playerLoco > 0 && IsClientConnected(playerLoco)) //player is already in 
+    {
+        std::cout << "ERROR: Player Already Connected! \n" << std::endl;
+        return false;
+    }
+
+    int freeSpot = FindFreeClientIndex();
+
+    if (freeSpot < 0) //lobby full 
+    {
+        std::cout << "ERROR: Lobby is full! \n" << std::endl;
+        return false;
+    }
+
+    playerAddress[freeSpot] = potentialPlayer;
+    playerConnected[freeSpot] = true;
+
+    return true;
 }
--- ScratchNetServer/Server.h
+++ ScratchNetServer/Server.h
@@ -2,13 +2,29 @@
 #include <queue>
 
 #include "Socket.h"
+#include <vector>
+#include <unordered_map>
 
 class Server
 {
 public:
     void MainProcess();
+
+    //returns the position the player is located otherwise -1
+    int FindPlayer(Address player);
+    int FindFreeClientIndex() const;
+    bool IsClientConnected(int clientIndex);
+    const Address& GetClientAddress(int clientIndex);
+
+    bool TryToAddPlayer(Address potentialPlayer);
     
 private:
     Socket listeningSocket;
     Socket actionSocket;
+
+    int maxPlayers = 4;
+    int numOfConnectedClients;
+
+    std::vector<bool> playerConnected;
+    std::vector<Address> playerAddress;
 };
